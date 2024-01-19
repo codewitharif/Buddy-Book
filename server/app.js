0d2c05d065cc9129c58b8de2340bc859c05e4b91
Md Arif <75657124+codewitharif@users.noreply.github.com>
2024-01-19 09:27:27 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -8,7 +8,11 @@ const crud = require("./models/crudSchema");
 const cors = require("cors");
 const router = require("./routes/router");
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://buddybookz.netlify.app/',
+    methods: [GET,PATCH,POST,DELETE],
+    credentials: true,
+}));
 app.use(express.json());
 
 app.use(router);
