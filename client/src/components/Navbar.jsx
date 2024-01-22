c621c0d771d9061dbb36a0ef52435aba14dc08f1
Md Arif <75657124+codewitharif@users.noreply.github.com>
2024-01-22 06:20:05 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <header>
-      <nav className="navbar navbar-expand-lg navbar bg-dark border-bottom border-body">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
             BuddyBook
